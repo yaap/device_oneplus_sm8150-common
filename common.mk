edd3c2c1459d44ecaf6887c438fd921b076a8bc8
Ajit Vaishya <ajitv@codeaurora.org>
2019-03-06 19:34:20 +0530

    sm8150-common: wifi: Default compile of WPA based module
    This commit introduce to provide default compilation
    for WPA based module.
    
    Change-Id: Ic0a1cd4383102dea83ea9c3d80177677ea2e59b7
    CRs-Fixed: 2410589
    Signed-off-by: Omkar Chandorkar <gotenksIN@aosip.dev>

--- common.mk
+++ common.mk
@@ -518,6 +518,7 @@ PRODUCT_PACKAGES += \
     vendor.qti.hardware.wifi.supplicant@2.1.vendor \
     wifi-mac-generator \
     WifiOverlay \
+    wpa_cli \
     wpa_supplicant \
     wpa_supplicant.conf
 
