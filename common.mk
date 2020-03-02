da30cff6d570536d66e330779c5ec4e65e6682b8
Nicholas Ambur <nambur@google.com>
2020-03-02 15:05:49 -0800

    sm8150-common: Add SoundTrigger HAL v2.3 support
    Bug: 139071862
    Test: gts-tradefed run gts-dev -m GtsAssistIntentTestCases
    Change-Id: I2d17aade762ceb7800d5305119c37ec0f5991f35

--- common.mk
+++ common.mk
@@ -123,7 +123,7 @@ PRODUCT_PACKAGES += \
     android.hardware.audio.service \
     android.hardware.audio@6.0-impl:32 \
     android.hardware.audio.effect@6.0-impl:32 \
-    android.hardware.soundtrigger@2.1-impl:32 \
+    android.hardware.soundtrigger@2.3-impl:32 \
     audio.primary.default \
     audio.primary.msmnile \
     audio.bluetooth.default \
