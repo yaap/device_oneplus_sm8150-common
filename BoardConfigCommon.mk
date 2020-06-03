ccd17b4bcdc22010eaa30fc95bd2ecd664671105
Jake Weinstein <jake@aospa.co>
2020-06-02 20:56:08 -0400

    sm8150-common: Add init script to set properties per variant.
     * Adapted from LineageOS.
    
    Change-Id: Ief5d98e41a5808d057096b25a6e743e608660e12

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -149,6 +149,10 @@ DEVICE_MATRIX_FILE := $(VENDOR_PATH)/compatibility_matrix.xml
 DEVICE_MANIFEST_FILE := $(VENDOR_PATH)/manifest.xml
 ODM_MANIFEST_FILES := $(VENDOR_PATH)/manifest-qva.xml
 
+# Init
+TARGET_INIT_VENDOR_LIB := //$(VENDOR_PATH):libinit_oneplus-sm8150
+TARGET_RECOVERY_DEVICE_MODULES := libinit_oneplus-sm8150
+
 # Keystore
 TARGET_PROVIDES_KEYMASTER := true
 
