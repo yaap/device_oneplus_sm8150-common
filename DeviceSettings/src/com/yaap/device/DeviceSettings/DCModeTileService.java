9f5bef8b55810150440253282af7bd2f71dc72af
Omkar Chandorkar <forumomkar@gmail.com>
2020-07-01 19:31:41 +0530

    sm8150-common: DeviceSettings: Update DC Dimming Tile icon
    Credits to @Wizper99 for icon
    
    Change-Id: I606c4307172734b652b511739fb588ad754e5225

--- DeviceSettings/src/com/yaap/device/DeviceSettings/DCModeTileService.java
+++ DeviceSettings/src/com/yaap/device/DeviceSettings/DCModeTileService.java
@@ -20,6 +20,7 @@ package com.yaap.device.DeviceSettings;
 import android.annotation.TargetApi;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.graphics.drawable.Icon;
 import android.service.quicksettings.Tile;
 import android.service.quicksettings.TileService;
 import androidx.preference.PreferenceManager;
@@ -50,6 +51,8 @@ public class DCModeTileService extends TileService {
         super.onStartListening();
         SharedPreferences sharedPrefs = PreferenceManager.getDefaultSharedPreferences(this);
         enabled = DCModeSwitch.isCurrentlyEnabled(this);
+        getQsTile().setIcon(Icon.createWithResource(this,
+                    enabled ? R.drawable.ic_dimming_on : R.drawable.ic_dimming_off));
         getQsTile().setState(enabled ? Tile.STATE_ACTIVE : Tile.STATE_INACTIVE);
         getQsTile().updateTile();
 
@@ -68,6 +71,8 @@ public class DCModeTileService extends TileService {
         Utils.writeValue(DCModeSwitch.getFile(), enabled ? "0" : "1");
         sharedPrefs.edit().putBoolean(DeviceSettings.KEY_DC_SWITCH, enabled ? false : true).commit();
         //getQsTile().setLabel(enabled ? "DC off" : "DC On");
+        getQsTile().setIcon(Icon.createWithResource(this,
+                    enabled ? R.drawable.ic_dimming_off : R.drawable.ic_dimming_on));
         getQsTile().setState(enabled ? Tile.STATE_INACTIVE : Tile.STATE_ACTIVE);
         getQsTile().updateTile();
     }
