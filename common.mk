c6ee9c63c6ac9ed1108bbca95651a84a04d0a4a6
Omkar Chandorkar <forumomkar@gmail.com>
2020-09-27 19:16:26 +0530

    sm8150-common: build gapps conditionally

--- common.mk
+++ common.mk
@@ -9,6 +9,10 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
 # Get non-open-source specific aspects
 $(call inherit-product-if-exists, vendor/oneplus/sm8150-common/sm8150-common-vendor.mk)
 
+#Gapps
+$(call inherit-product-if-exists, vendor/google/gms/config.mk)
+$(call inherit-product-if-exists, vendor/google/pixel/config.mk)
+
 # Additional native libraries
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/public.libraries.txt:$(TARGET_COPY_OUT_VENDOR)/etc/public.libraries.txt
