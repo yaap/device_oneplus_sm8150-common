ee2e398eaa3ef8874ff6db4317778599430cbf1e
Omkar Chandorkar <forumomkar@gmail.com>
2020-09-29 19:42:20 +0530

    sm8150-common: don't build cameraHIDL
    we ship prebuilt so can patch em

--- common.mk
+++ common.mk
@@ -170,7 +170,6 @@ PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-service_64 \
     libxml2 \
     Snap \
-    vendor.oneplus.camera.CameraHIDL@1.0.vendor \
     vendor.qti.hardware.camera.device@1.0.vendor
 
 # CNE
