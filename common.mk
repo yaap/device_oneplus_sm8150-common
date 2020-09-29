58850f9095a505f7a595d3473397f9e6cf3d6280
Omkar Chandorkar <forumomkar@gmail.com>
2020-09-29 19:50:18 +0530

    sm8150-common: build android.hidl.memory.block@1.0 for camera

--- common.mk
+++ common.mk
@@ -168,6 +168,7 @@ PRODUCT_PACKAGES += \
 # Camera
 PRODUCT_PACKAGES += \
     android.frameworks.displayservice@1.0 \
+    android.hidl.memory.block@1.0 \
     android.hardware.camera.provider@2.4-impl \
     android.hardware.camera.provider@2.4-service_64 \
     libxml2 \
