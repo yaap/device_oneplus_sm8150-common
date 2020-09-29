b5efdf3edfc40573a3438fa46e4a89c3f4c0ca95
Omkar Chandorkar <forumomkar@gmail.com>
2020-09-29 20:03:35 +0530

    Le roi est mort, vive le roi!
    (our meme way of saying aosip -> yaap)

--- common.mk
+++ common.mk
@@ -16,7 +16,7 @@ PRODUCT_COPY_FILES += \
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-aosip
+    $(LOCAL_PATH)/overlay-yaap
 
 PRODUCT_ENFORCE_RRO_TARGETS := *
 
