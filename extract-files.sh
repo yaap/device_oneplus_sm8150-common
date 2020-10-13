79ffc0d93e0265e12fd5899faeb30f40aa5958f9
Omkar Chandorkar <forumomkar@gmail.com>
2020-10-13 12:25:34 +0530

    sm8150-common: move patching fod blob to common for hotdogg
    adapt it to current blob_fixup

--- extract-files.sh
+++ extract-files.sh
@@ -48,6 +48,15 @@ function blob_fixup() {
         patchelf --replace-needed "libhidlbase.so" "libhidlbase-v29.so" "${2}"
     ;;
     esac
+    case "${DEVICE}" in
+        hotdogg )
+        case "${1}" in
+            vendor/lib/libgf_ud_hal.so | vendor/lib64/libgf_ud_hal.so )
+            sed -i "s|vendor.boot.verifiedbootstate|vendor.boot.fingerprintbstate|g" "${2}"
+            ;;
+            esac
+    ;;
+    esac
 }
 
 # Default to sanitizing the vendor folder before extraction
