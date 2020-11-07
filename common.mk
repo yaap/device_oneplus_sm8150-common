2f7784067a78b167ff3e55998b4b2195e30f80c9
Omkar Chandorkar <gotenksIN@aosip.dev>
2020-11-07 20:25:58 +0530

    sm8150-common: blob librmnetctl
    Signed-off-by: Omkar Chandorkar <gotenksIN@aosip.dev>

--- common.mk
+++ common.mk
@@ -404,7 +404,6 @@ PRODUCT_PACKAGES += \
     android.hardware.radio.config@1.0 \
     android.hardware.secure_element@1.0 \
     libprotobuf-cpp-full \
-    librmnetctl \
     libxml2
 
 # Seccomp policy
