b87b7f35970ca63ae167e24ac11b7b6dcf88c9a3
pramod kotreshappa <pramodk@codeaurora.org>
2020-12-03 11:06:15 +0530

    sm8150-common: Bluetooth: Push BT permission files to device Push Bluetooth and Bluetooth LE permission files for enabling BLE. Set MAX_ACL_CONNECTIONS and MAX_L2CAP_CONNECTIONS to 16
    Change-Id: I4768a66100c6b131d4421370732c9200fcdedcd8
    Signed-off-by: Omkar Chandorkar <gotenksIN@aosip.dev>

--- bluetooth/include/bdroid_buildcfg.h
+++ bluetooth/include/bdroid_buildcfg.h
@@ -70,6 +70,8 @@ static inline const char *BtmGetDefaultName()
 #define BTM_WBS_INCLUDED TRUE
 #define BTIF_HF_WBS_PREFERRED TRUE
 
+#define MAX_ACL_CONNECTIONS   16
+#define MAX_L2CAP_CHANNELS    16
 #define BLE_VND_INCLUDED   TRUE
 // skips conn update at conn completion
 #define BT_CLEAN_TURN_ON_DISABLED 1
