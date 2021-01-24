78ac4be7abffed476165f8d67741f006e0a6b94f
Omkar Chandorkar <gotenksIN@aosip.dev>
2021-01-24 09:22:51 +0530

    sm8150-common: update widevine DRM from OB1
    * OP7Pro qssi-user-11-RKQ1.201022.002-2101120555-release-keys
    
    Signed-off-by: Omkar Chandorkar <gotenksIN@aosip.dev>

--- common.mk
+++ common.mk
@@ -262,10 +262,7 @@ PRODUCT_PACKAGES += \
     OnePlusDoze
 
 # DRM
-PRODUCT_PACKAGES += \
-    android.hardware.drm@1.0-impl \
-    android.hardware.drm@1.0-service \
-    android.hardware.drm@1.3-service.clearkey
+PRODUCT_PACKAGES += android.hardware.drm@1.3-service.clearkey
 
 # Fingerprint
 PRODUCT_PACKAGES += \
