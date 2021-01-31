ff427f044b4706d44c10325fab45c0649d21b2ed
Omkar Chandorkar <gotenksIN@aosip.dev>
2021-01-31 22:05:21 +0530

    sm8150-common: update NFC from OB1
    * also unpin NxpNfcNci.apk since the introduction of OpFeatures
    
    Signed-off-by: Omkar Chandorkar <gotenksIN@aosip.dev>

--- common.mk
+++ common.mk
@@ -367,7 +367,6 @@ PRODUCT_PACKAGES += \
     android.hardware.nfc@1.1:64 \
     android.hardware.nfc@1.2:64 \
     android.hardware.secure_element@1.0:64 \
-    android.hardware.secure_element@1.1:64 \
     com.android.nfc_extras \
     Tag \
     vendor.nxp.nxpese@1.0:64 \
