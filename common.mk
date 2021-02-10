076b0d5e2bdcdddd45e65e9b11ea61c3d88e655a
Omkar Chandorkar <gotenksIN@aosip.dev>
2021-02-10 12:25:49 +0530

    sm8150-common: commonize listen blobs and remove unused 64bit libs
    Signed-off-by: Omkar Chandorkar <gotenksIN@aosip.dev>

--- common.mk
+++ common.mk
@@ -111,7 +111,7 @@ PRODUCT_PACKAGES += \
     android.hardware.audio.effect@5.0-impl \
     android.hardware.audio.common@2.0-util \
     android.hardware.audio.common@5.0-util \
-    android.hardware.soundtrigger@2.1-impl \
+    android.hardware.soundtrigger@2.1-impl:32 \
     android.hardware.bluetooth.audio@2.0-impl \
     audio.a2dp.default \
     audio.bluetooth.default \
