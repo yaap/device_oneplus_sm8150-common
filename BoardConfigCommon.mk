a95ed1fea8537dbcb3e72b96158d170570876173
Omkar Chandorkar <gotenksIN@aosip.dev>
2021-04-06 12:21:18 +0530

    sm8150-common: include manifest-qva for sku_3 as well
    Signed-off-by: Omkar Chandorkar <gotenksIN@aosip.dev>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -146,7 +146,9 @@ DEVICE_MANIFEST_FILE += $(VENDOR_PATH)/manifest.xml
 ODM_MANIFEST_FILES += $(VENDOR_PATH)/manifest-qva.xml
 
 ODM_MANIFEST_SKUS += 3
-ODM_MANIFEST_3_FILES += $(VENDOR_PATH)/manifest_3.xml
+ODM_MANIFEST_3_FILES := \
+    $(VENDOR_PATH)/manifest-qva.xml \
+    $(VENDOR_PATH)/manifest_3.xml
 
 # Init
 TARGET_INIT_VENDOR_LIB := //$(VENDOR_PATH):libinit_oneplus-sm8150
