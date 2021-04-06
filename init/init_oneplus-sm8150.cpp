d80c497316e775e5cb5224901e4319beb2e8a82b
Ido Ben-Hur <idoybh2@gmail.com>
2021-04-06 04:30:39 +0300

    sm8150-comon: init: update fingerprint from coral

--- init/init_oneplus-sm8150.cpp
+++ init/init_oneplus-sm8150.cpp
@@ -89,5 +89,5 @@ void vendor_load_properties() {
   load_dalvikvm_properties();
 
   // fingerprint
-  property_override_multi("ro.build.fingerprint", "ro.vendor.build.fingerprint","ro.bootimage.build.fingerprint", "google/coral/coral:11/RQ2A.210305.006/7119741:user/release-keys");
+  property_override_multi("ro.build.fingerprint", "ro.vendor.build.fingerprint","ro.bootimage.build.fingerprint", "google/coral/coral:11/RQ2A.210405.005/7181113:user/release-keys");
 }
