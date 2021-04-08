a3b6d5e9343ef9f7943fd8134209403dac07d7f3
mauronofrio <poronofrio@gmail.com>
2021-04-08 14:36:12 +0530

    sm8150-common: libinit: add proper model detection for OP7/P/T
    * helps getting back HDR on Netflix
    
    Co-authored-by: Omkar Chandorkar <gotenksIN@aosip.dev>
    Signed-off-by: Omkar Chandorkar <gotenksIN@aosip.dev>

--- init/init_oneplus-sm8150.cpp
+++ init/init_oneplus-sm8150.cpp
@@ -84,7 +84,144 @@ void load_dalvikvm_properties() {
   property_override("dalvik.vm.heapminfree", "8m");
 }
 
+void load_op7(const char *model) {
+  property_override("ro.product.model", model);
+  property_override("ro.product.name", "OnePlus7");
+  property_override("ro.build.product", "OnePlus7");
+  property_override("ro.product.device", "OnePlus7");
+  property_override("ro.vendor.product.device", "OnePlus7");
+  property_override("ro.display.series", "OnePlus 7");
+}
+
+void load_op7pro(const char *model) {
+  property_override("ro.product.model", model);
+  property_override("ro.product.name", "OnePlus7Pro");
+  property_override("ro.build.product", "OnePlus7Pro");
+  property_override("ro.product.device", "OnePlus7Pro");
+  property_override("ro.vendor.product.device", "OnePlus7Pro");
+  property_override("ro.display.series", "OnePlus 7 Pro");
+}
+
+void load_op7t(const char *model) {
+  property_override("ro.product.model", model);
+  property_override("ro.product.name", "OnePlus7T");
+  property_override("ro.build.product", "OnePlus7T");
+  property_override("ro.product.device", "OnePlus7T");
+  property_override("ro.vendor.product.device", "OnePlus7T");
+  property_override("ro.display.series", "OnePlus 7T");
+}
+
+void load_op7tpro(const char *model) {
+  property_override("ro.product.model", model);
+  property_override("ro.product.name", "OnePlus7TPro");
+  property_override("ro.build.product", "OnePlus7TPro");
+  property_override("ro.product.device", "OnePlus7TPro");
+  property_override("ro.vendor.product.device", "OnePlus7TPro");
+  property_override("ro.display.series", "OnePlus 7T Pro");
+}
+
 void vendor_load_properties() {
+  int project_name = stoi(android::base::GetProperty("ro.boot.project_name", ""));
+  int rf_version = stoi(android::base::GetProperty("ro.boot.rf_version", ""));
+  switch(project_name){
+    case 18857:
+      switch (rf_version){
+        case 1:
+          /* China*/
+          load_op7("GM1900");
+          break;
+        case 3:
+          /* India*/
+          load_op7("GM1901");
+          break;
+        case 4:
+          /* Europe */
+          load_op7("GM1903");
+          break;
+        case 5:
+          /* Global / US Unlocked */
+          load_op7("GM1907");
+          break;
+        default:
+          /* Generic */
+          load_op7("GM1907");
+          break;
+      }
+      break;
+    case 18821:
+      switch (rf_version){
+        case 1:
+          /* China*/
+          load_op7pro("GM1910");
+          break;
+        case 3:
+          /* India*/
+          load_op7pro("GM1911");
+          break;
+        case 4:
+          /* Europe */
+          load_op7pro("GM1913");
+          break;
+        case 5:
+          /* Global / US Unlocked */
+          load_op7pro("GM1917");
+          break;
+        default:
+          /* Generic*/
+          load_op7pro("GM1917");
+          break;
+      }
+      break;
+    case 18865:
+      switch (rf_version){
+        case 1:
+          /* China*/
+          load_op7t("HD1900");
+          break;
+        case 3:
+          /* India*/
+          load_op7t("HD1901");
+          break;
+        case 4:
+          /* Europe */
+          load_op7t("HD1903");
+          break;
+        case 5:
+          /* Global / US Unlocked */
+          load_op7t("HD1907");
+          break;
+        default:
+          /* Generic */
+          load_op7t("HD1907");
+          break;
+      }
+      break;
+      case 19801:
+      switch (rf_version){
+        case 1:
+          /* China*/
+          load_op7tpro("HD1910");
+          break;
+        case 3:
+          /* India*/
+          load_op7tpro("HD1911");
+          break;
+        case 4:
+          /* Europe */
+          load_op7tpro("HD1913");
+          break;
+        case 5:
+          /* Global / US Unlocked */
+          load_op7tpro("HD1917");
+          break;
+        default:
+          /* Generic*/
+          load_op7tpro("HD1917");
+          break;
+      }
+      break;
+  }
+
   // dalvikvm props
   load_dalvikvm_properties();
 
