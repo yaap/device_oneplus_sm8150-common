3b1230fb0197c1b011157a12352311a50b291ad0
Omkar Chandorkar <gotenksIN@aosip.dev>
2021-04-14 10:51:55 +0530

    sm8150-common: BoardConfig: switch to Image.gz-dtb
    Signed-off-by: Omkar Chandorkar <gotenksIN@aosip.dev>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -40,7 +40,7 @@ BOARD_BOOT_HEADER_VERSION := 2
 BOARD_KERNEL_BASE := 0x00000000
 BOARD_KERNEL_CMDLINE := androidboot.hardware=qcom androidboot.console=ttyMSM0 androidboot.memcg=1 lpm_levels.sleep_disabled=1 video=vfb:640x400,bpp=32,memsize=3072000 msm_rtb.filter=0x237 service_locator.enable=1 swiotlb=2048 loop.max_part=7 androidboot.usbcontroller=a600000.dwc3
 BOARD_KERNEL_CMDLINE += androidboot.vbmeta.avb_version=1.0
-BOARD_KERNEL_IMAGE_NAME := Image-dtb
+BOARD_KERNEL_IMAGE_NAME := Image.gz-dtb
 BOARD_KERNEL_PAGESIZE := 4096
 BOARD_KERNEL_SEPARATED_DTBO := true
 BOARD_KERNEL_TAGS_OFFSET := 0x01E00000
