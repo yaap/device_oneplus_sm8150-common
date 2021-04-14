af41cd292a1cab55732ff9888d353b10b1bdc2c6
LuK1337 <priv.luk@gmail.com>
2021-04-14 14:29:58 +0200

    sm8150-common: Import QTI vibrator HAL
    Change-Id: I6ed3252c6da746c8fb24f44b3b3ec2b1f32a6cb2

--- common.mk
+++ common.mk
@@ -400,6 +400,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     libstdc++.vendor
 
+# Vibrator
+PRODUCT_PACKAGES += \
+    vendor.qti.hardware.vibrator.service.oneplus_msmnile
+
 # Seccomp policy
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/seccomp/atfwd@2.0.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/atfwd@2.0.policy \
