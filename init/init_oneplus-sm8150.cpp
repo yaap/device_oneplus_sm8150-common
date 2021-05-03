b797d361479e884a3df56ad88b1822d55cb31bb6
Omkar Chandorkar <gotenksIN@aosip.dev>
2021-05-03 17:35:49 +0000

    sm8150-common: libinit: update to may coral fp
    Signed-off-by: Omkar Chandorkar <gotenksIN@aosip.dev>

--- init/init_oneplus-sm8150.cpp
+++ init/init_oneplus-sm8150.cpp
@@ -202,5 +202,5 @@ void vendor_load_properties() {
   load_dalvikvm_properties();
 
   // fingerprint
-  property_override_multi("ro.build.fingerprint", "ro.vendor.build.fingerprint","ro.bootimage.build.fingerprint", "google/coral/coral:11/RQ2A.210405.005/7181113:user/release-keys");
+  property_override_multi("ro.build.fingerprint", "ro.vendor.build.fingerprint","ro.bootimage.build.fingerprint", "google/coral/coral:11/RQ2A.210505.002/7246365:user/release-keys");
 }
