5d947faf8359deef15265052dbb174834382ec81
Omkar Chandorkar <gotenksIN@aosip.dev>
2021-05-16 15:34:54 +0000

    sm8150-common: sync media props with CAF
    Signed-off-by: Omkar Chandorkar <gotenksIN@aosip.dev>

--- common.mk
+++ common.mk
@@ -322,6 +322,9 @@ PRODUCT_COPY_FILES += \
     frameworks/av/media/libstagefright/data/media_codecs_google_video.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_video.xml \
     frameworks/av/media/libstagefright/data/media_codecs_google_video_le.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_video_le.xml
 
+PRODUCT_COPY_FILES += \
+    hardware/qcom-caf/sm8150/media/conf_files/msmnile/system_properties.xml:$(TARGET_COPY_OUT_VENDOR)/etc/system_properties.xml
+
 # Net
 PRODUCT_PACKAGES += \
     netutils-wrapper-1.0
@@ -345,6 +348,7 @@ PRODUCT_PACKAGES += \
     libc2dcolorconvert \
     libcodec2_hidl@1.0.vendor \
     libcodec2_vndk.vendor \
+    libmm-omxcore \
     libOmxAacEnc \
     libOmxAmrEnc \
     libOmxCore \
@@ -354,7 +358,8 @@ PRODUCT_PACKAGES += \
     libOmxVdec \
     libOmxVenc \
     libOmxVidcCommon \
-    libstagefrighthw
+    libstagefrighthw \
+    libstagefright_softomx.vendor
 
 # OnePlus
 PRODUCT_PACKAGES += \
