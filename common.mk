88aa85a326133be70659ee7e5a43a856fd4063cc
Hikari-no-Tenshi <kyryljan.serhij@gmail.com>
2021-05-19 10:32:12 +0300

    sm8150-common: update camera blobs
    Signed-off-by: Omkar Chandorkar <gotenksIN@aosip.dev>

--- common.mk
+++ common.mk
@@ -165,7 +165,7 @@ PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-service_64 \
     libxml2 \
     Snap \
-    vendor.oneplus.camera.CameraHIDL@1.0.vendor:64 \
+    vendor.oneplus.hardware.camera@1.0.vendor:64 \
     vendor.qti.hardware.camera.device@1.0.vendor:64
 
 # Common init scripts
