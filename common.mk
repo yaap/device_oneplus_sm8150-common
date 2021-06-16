55e3361b758fd842e9eccf3133c6c07424eb72d1
Omkar Chandorkar <gotenksIN@aosip.dev>
2021-06-16 04:56:14 +0200

    sm8150-common: drop in-tree biometric overlay
    Signed-off-by: Omkar Chandorkar <gotenksIN@aosip.dev>

--- common.mk
+++ common.mk
@@ -19,13 +19,9 @@ PRODUCT_COPY_FILES += \
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-system \
     $(LOCAL_PATH)/overlay-yaap
 
 PRODUCT_ENFORCE_RRO_TARGETS := *
-
-PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += \
-    $(LOCAL_PATH)/overlay-system
 
 # A/B
 AB_OTA_POSTINSTALL_CONFIG += \
