628d40812908e9359ef6a0e37fef460b28663f60
Luca Stefani <luca.stefani.ge1@gmail.com>
2021-06-30 18:09:54 +0200

    sm8150-common: Use uncompressed dtbless kernel image
    * Apparently this is what stock does too.
    
    Signed-off-by: Omkar Chandorkar <gotenksIN@aosip.dev>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -34,7 +34,7 @@ BOARD_BOOT_HEADER_VERSION := 2
 BOARD_KERNEL_BASE := 0x00000000
 BOARD_KERNEL_CMDLINE := androidboot.hardware=qcom androidboot.console=ttyMSM0 androidboot.memcg=1 lpm_levels.sleep_disabled=1 msm_rtb.filter=0x237 service_locator.enable=1 swiotlb=2048 loop.max_part=7 androidboot.usbcontroller=a600000.dwc3  kpti=off
 BOARD_KERNEL_CMDLINE += androidboot.vbmeta.avb_version=1.0
-BOARD_KERNEL_IMAGE_NAME := Image.gz-dtb
+BOARD_KERNEL_IMAGE_NAME := Image
 BOARD_KERNEL_PAGESIZE := 4096
 BOARD_KERNEL_SEPARATED_DTBO := true
 BOARD_MKBOOTIMG_ARGS += --header_version $(BOARD_BOOT_HEADER_VERSION)
