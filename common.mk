8ec303c19ced6efa0d9d28715dd9bf5cb76f7139
Dyneteve <dyneteve@hentaios.com>
2021-10-27 08:55:17 +0530

    sm8150-common: Build vendor HIDL interfaces
    * In a perfect world google expects us to build all the hals from source however this isn't the case here.
    The HIDL interfaces got removed by default from VNDK and moved to vendor so as a result we need to build them ourselves
    
    Signed-off-by: Omkar Chandorkar <gotenksIN@aosip.dev>

--- common.mk
+++ common.mk
@@ -12,6 +12,9 @@ $(call inherit-product, vendor/oneplus/sm8150-common/sm8150-common-vendor.mk)
 # Project ID Quota
 $(call inherit-product, $(SRC_TARGET_DIR)/product/emulated_storage.mk)
 
+# Interfaces
+$(call inherit-product, $(LOCAL_PATH)/interfaces.mk)
+
 # Additional native libraries
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/public.libraries.txt:$(TARGET_COPY_OUT_VENDOR)/etc/public.libraries.txt
---
+++ interfaces.mk
@@ -0,0 +1,18 @@
+PRODUCT_PACKAGES += \
+    android.hardware.bluetooth.a2dp@1.0.vendor \
+    android.hardware.bluetooth@1.1.vendor \
+    android.hardware.drm@1.4.vendor \
+    android.hardware.gatekeeper@1.0.vendor \
+    android.hardware.keymaster@4.1.vendor \
+    android.system.net.netd@1.1.vendor \
+    android.hardware.neuralnetworks@1.3.vendor \
+    android.hardware.radio@1.5.vendor \
+    android.hardware.radio.config@1.2.vendor \
+    android.hardware.radio.deprecated@1.0.vendor \
+    android.frameworks.sensorservice@1.0.vendor \
+    android.hardware.wifi@1.5.vendor \
+    android.hardware.power@1.2.vendor \
+    android.hardware.gnss@2.1.vendor \
+    android.hardware.secure_element@1.2.vendor \
+    android.hardware.audio@4.0.vendor:64 \
+    android.hardware.nfc@1.2.vendor
