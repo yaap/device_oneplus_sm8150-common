dac22765d29ff16bbcdefc7291407794060f7424
Ahmed Harhash <Lacentix@pm.me>
2021-11-11 21:38:31 +0200

    sm8150-common: init: Change fingerprint from coral to walleye oreo
    * Hopefully this passes sn

--- init/init_oneplus-sm8150.cpp
+++ init/init_oneplus-sm8150.cpp
@@ -207,5 +207,5 @@ void vendor_load_properties() {
   load_dalvikvm_properties();
 
   // fingerprint
-  property_override_multi("ro.build.fingerprint", "ro.vendor.build.fingerprint","ro.bootimage.build.fingerprint", "google/coral/coral:11/RQ3A.211001.001/7641976:user/release-keys");
+  property_override_multi("ro.build.fingerprint", "ro.vendor.build.fingerprint","ro.bootimage.build.fingerprint", "google/walleye/walleye:8.1.0/OPM1.171019.011/4448085:user/release-keys");
 }
