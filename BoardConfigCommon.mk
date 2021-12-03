b46e5d68ccfc4422f9af57614e66b71ad99224c7
Dyneteve <dyneteve@hentaios.com>
2021-12-03 21:39:39 +0530

    sm8150-common: Re-configure CPU variant according to GS101
    Signed-off-by: Omkar Chandorkar <gotenksIN@aosip.dev>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -12,16 +12,19 @@ VENDOR_PATH := device/oneplus/sm8150-common
 
 # Architecture
 TARGET_ARCH := arm64
-TARGET_ARCH_VARIANT := armv8-2a
+TARGET_ARCH_VARIANT := armv8-2a-dotprod
 TARGET_CPU_ABI := arm64-v8a
-TARGET_CPU_ABI2 :=
-TARGET_CPU_VARIANT := cortex-a76
+TARGET_CPU_VARIANT := cortex-a55
+TARGET_CPU_VARIANT_RUNTIME := cortex-a55
 
+ifeq (,$(filter %_64,$(TARGET_PRODUCT)))
 TARGET_2ND_ARCH := arm
-TARGET_2ND_ARCH_VARIANT := armv8-2a
+TARGET_2ND_ARCH_VARIANT := armv8-a
 TARGET_2ND_CPU_ABI := armeabi-v7a
 TARGET_2ND_CPU_ABI2 := armeabi
-TARGET_2ND_CPU_VARIANT := cortex-a76
+TARGET_2ND_CPU_VARIANT := generic
+TARGET_2ND_CPU_VARIANT_RUNTIME := cortex-a53
+endif
 
 TARGET_USES_64_BIT_BINDER := true
 
