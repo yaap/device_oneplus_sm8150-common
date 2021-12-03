cdf8851593650efbc9a7fd6ea83487a03397b1af
Ido Ben-Hur <idoybh2@gmail.com>
2021-12-03 05:54:43 +0200

    camera_helper: Show alert dialog before raising when screen is off
    Also bring back always show, imporve a bit of code around.
    Idea, dialog and dialog strings taken from: https://gerrit.aospa.co/c/AOSPA/android_frameworks_base/+/9456
    Rewrote for non intent based camera_helper, giving co-authorship
    
    Co-authored-by: rituj <ritujbeniwal@gmail.com>

--- camera_helper/src/org/lineageos/camerahelper/CameraMotorService.java
+++ camera_helper/src/org/lineageos/camerahelper/CameraMotorService.java
@@ -17,18 +17,27 @@
 package org.lineageos.camerahelper;
 
 import android.annotation.NonNull;
+import android.app.AlertDialog;
 import android.app.Service;
+import android.content.Context;
 import android.content.Intent;
 import android.hardware.camera2.CameraManager;
+import android.hardware.display.DisplayManager;
 import android.os.Handler;
 import android.os.IBinder;
 import android.os.Message;
 import android.os.SystemClock;
+import android.provider.Settings;
 import android.util.Log;
+import android.view.Display;
+import android.view.WindowManager;
 
 public class CameraMotorService extends Service implements Handler.Callback {
     private static final boolean DEBUG = true;
     private static final String TAG = "CameraMotorService";
+
+    // Should follow KEY_SETTINGS_PREFIX + KEY_ALWAYS_CAMERA_DIALOG From DeviceSettings
+    private static final String ALWAYS_ON_DIALOG_KEY = "device_setting_always_on_camera_dialog";
 
     public static final int CAMERA_EVENT_DELAY_TIME = 100; // ms
 
@@ -37,12 +46,15 @@ public class CameraMotorService extends Service implements Handler.Callback {
     public static final int MSG_CAMERA_CLOSED = 1000;
     public static final int MSG_CAMERA_OPEN = 1001;
 
-    private Handler mHandler = new Handler(this);
+    private final Handler mHandler = new Handler(this);
+
+    private AlertDialog mAlertDialog;
+    private DisplayManager mDisplayManager;
 
     private long mClosedEvent;
     private long mOpenEvent;
 
-    private CameraManager.AvailabilityCallback mAvailabilityCallback =
+    private final CameraManager.AvailabilityCallback mAvailabilityCallback =
             new CameraManager.AvailabilityCallback() {
                 @Override
                 public void onCameraClosed(@NonNull String cameraId) {
@@ -81,6 +93,8 @@ public class CameraMotorService extends Service implements Handler.Callback {
 
         CameraManager cameraManager = getSystemService(CameraManager.class);
         cameraManager.registerAvailabilityCallback(mAvailabilityCallback, null);
+
+        mDisplayManager = (DisplayManager) getSystemService(Context.DISPLAY_SERVICE);
     }
 
     @Override
@@ -104,14 +118,59 @@ public class CameraMotorService extends Service implements Handler.Callback {
     public boolean handleMessage(Message msg) {
         switch (msg.what) {
             case MSG_CAMERA_CLOSED:
-                CameraMotorController.setMotorDirection(CameraMotorController.DIRECTION_DOWN);
-                CameraMotorController.setMotorEnabled();
+                lowerCamera();
                 break;
             case MSG_CAMERA_OPEN:
-                CameraMotorController.setMotorDirection(CameraMotorController.DIRECTION_UP);
-                CameraMotorController.setMotorEnabled();
+                maybeRaiseCamera();
                 break;
         }
         return true;
+    }
+
+    private void maybeRaiseCamera() {
+        boolean screenOn = false;
+        for (Display display : mDisplayManager.getDisplays()) {
+            if (display.getState() != Display.STATE_OFF) {
+                screenOn = true;
+                break;
+            }
+        }
+        boolean alwaysOnDialog = Settings.System.getInt(getContentResolver(),
+                ALWAYS_ON_DIALOG_KEY, 0) == 1;
+        if (screenOn && !alwaysOnDialog) {
+            raiseCamera();
+        } else {
+            if (mAlertDialog == null) {
+                mAlertDialog = new AlertDialog.Builder(this)
+                        .setMessage(R.string.popup_camera_dialog_message)
+                        .setNegativeButton(R.string.popup_camera_dialog_no,
+                                (dialog, which) -> {
+                            // Go back to home screen
+                            Intent intent = new Intent(Intent.ACTION_MAIN);
+                            intent.addCategory(Intent.CATEGORY_HOME);
+                            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                            this.startActivity(intent);
+                        })
+                        .setPositiveButton(R.string.popup_camera_dialog_raise,
+                                (dialog, which) -> raiseCamera())
+                        .create();
+                mAlertDialog.getWindow().setType(WindowManager.LayoutParams.TYPE_SYSTEM_ERROR);
+                mAlertDialog.setCanceledOnTouchOutside(false);
+            }
+            if (!mAlertDialog.isShowing()) mAlertDialog.show();
+        }
+    }
+
+    private void raiseCamera() {
+        if (DEBUG) Log.d(TAG, "Raising camera");
+        CameraMotorController.setMotorDirection(CameraMotorController.DIRECTION_UP);
+        CameraMotorController.setMotorEnabled();
+    }
+
+    private void lowerCamera() {
+        if (DEBUG) Log.d(TAG, "Lowering camera");
+        if (mAlertDialog != null && mAlertDialog.isShowing()) mAlertDialog.dismiss();
+        CameraMotorController.setMotorDirection(CameraMotorController.DIRECTION_DOWN);
+        CameraMotorController.setMotorEnabled();
     }
 }
