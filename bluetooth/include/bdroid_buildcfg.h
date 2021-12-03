4b335633a6cc78a3bfd2f04437314f0553474726
Omkar Chandorkar <gotenksIN@aosip.dev>
2021-12-03 22:07:17 +0530

    sm8150-common: switch to aosp bluetooth
    Signed-off-by: Omkar Chandorkar <gotenksIN@aosip.dev>

--- bluetooth/include/bdroid_buildcfg.h
+++ bluetooth/include/bdroid_buildcfg.h
@@ -69,11 +69,9 @@ static inline const char *BtmGetDefaultName()
 
 #define BTM_DEF_LOCAL_NAME BtmGetDefaultName()
 // Disables read remote device feature
-#define MAX_ACL_CONNECTIONS   16
-#define MAX_L2CAP_CHANNELS    32
-#define BLE_VND_INCLUDED   TRUE
-#define GATT_MAX_PHY_CHANNEL  10
+#define BTM_WBS_INCLUDED TRUE
+#define BTIF_HF_WBS_PREFERRED TRUE
 
-#define AVDT_NUM_SEPS 35
+#define BLE_VND_INCLUDED   TRUE
 
 #endif
