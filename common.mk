accfa27a041143ba7f95af315139697795f0bcc7
Omkar Chandorkar <gotenksIN@aosip.dev>
2021-12-03 22:19:14 +0530

    sm8150-common: drop oneplus-fwk
    Signed-off-by: Omkar Chandorkar <gotenksIN@aosip.dev>

--- common.mk
+++ common.mk
@@ -345,13 +345,6 @@ PRODUCT_PACKAGES += \
     libstagefrighthw \
     libstagefright_softomx.vendor
 
-# OnePlus
-PRODUCT_PACKAGES += \
-    oneplus-fwk.oneplus_msmnile
-
-PRODUCT_BOOT_JARS += \
-    oneplus-fwk.oneplus_msmnile
-
 # Power
 PRODUCT_PACKAGES += \
     android.hardware.power-service-qti \
