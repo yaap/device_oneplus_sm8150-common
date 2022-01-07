3d8150c23544bad45d3488c1bc4350fbdb1cccb9
9ary <streetwalkermc@gmail.com>
2022-01-08 00:45:12 +0800

    sm8150-common: implement ALS compensation algorithm
    Signed-off-by: Pranav <npv12@iitbbs.ac.in>

--- sensors/als_correction_service.cpp
+++ sensors/als_correction_service.cpp
@@ -21,8 +21,10 @@
 
 #include <cstdio>
 #include <signal.h>
-#include <time.h>
 #include <unistd.h>
+#include <sysutils/FrameworkCommand.h>
+#include <sysutils/FrameworkListener.h>
+#include <utils/Timers.h>
 
 using android::base::SetProperty;
 using android::GraphicBuffer;
@@ -32,78 +34,81 @@ using android::sp;
 using android::SurfaceComposerClient;
 using namespace android;
 
-constexpr int ALS_RADIUS = 64;
-constexpr int SCREENSHOT_INTERVAL = 1;
+static Rect screenshot_rect(251, 988, 305, 1042);
 
-void updateScreenBuffer() {
-    static time_t lastScreenUpdate = 0;
-    static sp<GraphicBuffer> outBuffer = new GraphicBuffer(
-            10, 10, android::PIXEL_FORMAT_RGB_888,
-            GraphicBuffer::USAGE_SW_READ_OFTEN | GraphicBuffer::USAGE_SW_WRITE_OFTEN);
+class TakeScreenshotCommand : public FrameworkCommand {
+  public:
+    TakeScreenshotCommand() : FrameworkCommand("take_screenshot") {}
+    ~TakeScreenshotCommand() override = default;
 
-    struct timespec now;
-    clock_gettime(CLOCK_MONOTONIC, &now);
-    sp<SyncScreenCaptureListener> captureListener = new SyncScreenCaptureListener();
-    gui::ScreenCaptureResults captureResults;
+    int runCommand(SocketClient* cli, int /*argc*/, char **/*argv*/) {
+        auto screenshot = takeScreenshot();
+        cli->sendData(&screenshot, sizeof(screenshot_t));
+        return 0;
+    }
+  private:
+    struct screenshot_t {
+        uint32_t r, g, b;
+        nsecs_t timestamp;
+    };
 
-    if (now.tv_sec - lastScreenUpdate >= SCREENSHOT_INTERVAL) {
-        // Update Screenshot at most every second
+    screenshot_t takeScreenshot() {
+        static sp<GraphicBuffer> outBuffer = new GraphicBuffer(
+                screenshot_rect.getWidth(), screenshot_rect.getHeight(),
+                android::PIXEL_FORMAT_RGB_888,
+                GraphicBuffer::USAGE_SW_READ_OFTEN | GraphicBuffer::USAGE_SW_WRITE_OFTEN);
+
+        sp<SyncScreenCaptureListener> captureListener = new SyncScreenCaptureListener();
+        gui::ScreenCaptureResults captureResults;
+
         DisplayCaptureArgs captureArgs;
         captureArgs.displayToken = SurfaceComposerClient::getInternalDisplayToken();
-        captureArgs.pixelFormat = ui::PixelFormat::RGBA_8888;
-        captureArgs.sourceCrop = Rect(ALS_POS_X - ALS_RADIUS, ALS_POS_Y - ALS_RADIUS, ALS_POS_X + ALS_RADIUS, ALS_POS_Y + ALS_RADIUS);
-        captureArgs.width = ALS_RADIUS * 2;
-        captureArgs.height = ALS_RADIUS * 2;
-        captureArgs.useIdentityTransform = true;
-        status_t result = ScreenshotClient::captureDisplay(captureArgs, captureListener);
-        if (result == NO_ERROR) {
+        captureArgs.pixelFormat = android::ui::PixelFormat::RGBA_8888;
+        captureArgs.sourceCrop = screenshot_rect;
+        captureArgs.width = screenshot_rect.getWidth();
+        captureArgs.height = screenshot_rect.getHeight();
+        captureArgs.useIdentityTransform = false;
+        status_t ret = ScreenshotClient::captureDisplay(captureArgs, captureListener);
+        if (ret == NO_ERROR) {
             captureResults = captureListener->waitForResults();
-            if (captureResults.result == NO_ERROR) outBuffer = captureResults.buffer;
+            if (captureResults.result == NO_ERROR)  outBuffer = captureResults.buffer;
         }
-        lastScreenUpdate = now.tv_sec;
-    }
 
-    uint8_t *out;
-    auto resultWidth = outBuffer->getWidth();
-    auto resultHeight = outBuffer->getHeight();
-    auto stride = outBuffer->getStride();
+        uint8_t *out;
+        auto resultWidth = outBuffer->getWidth();
+        auto resultHeight = outBuffer->getHeight();
+        auto stride = outBuffer->getStride();
 
-    outBuffer->lock(GraphicBuffer::USAGE_SW_READ_OFTEN, reinterpret_cast<void **>(&out));
-    // we can sum this directly on linear light
-    uint32_t rsum = 0, gsum = 0, bsum = 0;
-    for (int y = 0; y < resultHeight; y++) {
-        for (int x = 0; x < resultWidth; x++) {
-            rsum += out[y * (stride * 4) + x * 4];
-            gsum += out[y * (stride * 4) + x * 4 + 1];
-            bsum += out[y * (stride * 4) + x * 4 + 2];
+        outBuffer->lock(GraphicBuffer::USAGE_SW_READ_OFTEN, reinterpret_cast<void **>(&out));
+        // we can sum this directly on linear light
+        uint32_t rsum = 0, gsum = 0, bsum = 0;
+        for (int y = 0; y < resultHeight; y++) {
+            for (int x = 0; x < resultWidth; x++) {
+                rsum += out[y * (stride * 4) + x * 4];
+                gsum += out[y * (stride * 4) + x * 4 + 1];
+                bsum += out[y * (stride * 4) + x * 4 + 2];
+            }
         }
+        uint32_t max = resultWidth * resultHeight;
+        outBuffer->unlock();
+
+        return { rsum / max, gsum / max, bsum / max, systemTime(SYSTEM_TIME_BOOTTIME) };
     }
-    uint32_t max = 255 * resultWidth * resultHeight;
-    SetProperty("vendor.sensors.als_correction.r", std::to_string(rsum * 0x7FFFFFFFuLL / max));
-    SetProperty("vendor.sensors.als_correction.g", std::to_string(gsum * 0x7FFFFFFFuLL / max));
-    SetProperty("vendor.sensors.als_correction.b", std::to_string(bsum * 0x7FFFFFFFuLL / max));
-    outBuffer->unlock();
-}
+};
+
+class AlsCorrectionListener : public FrameworkListener {
+  public:
+    AlsCorrectionListener() : FrameworkListener("als_correction") {
+        registerCmd(new TakeScreenshotCommand);
+    }
+};
 
 int main() {
     ProcessState::self()->setThreadPoolMaxThreadCount(0);
     ProcessState::self()->startThreadPool();
 
-    struct sigaction action{};
-    sigfillset(&action.sa_mask);
-
-    action.sa_flags = SA_RESTART;
-    action.sa_handler = [](int signal) {
-        if (signal == SIGUSR1) {
-            updateScreenBuffer();
-        }
-    };
-
-    if (sigaction(SIGUSR1, &action, nullptr) == -1) {
-        return -1;
-    }
-
-    SetProperty("vendor.sensors.als_correction.pid", std::to_string(getpid()));
+    auto listener = new AlsCorrectionListener();
+    listener->startListener();
 
     while (true) {
         pause();
