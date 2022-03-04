6b95e34f4b3d965de764d2585b2c8bea34f58bc1
Etienne Ruffieux <eruffieux@google.com>
2022-03-04 13:34:57 +0000

    sm8150-common: Removed audio A2dp from device.mk
    Bug: 220306859
    Test: atest
    Tag: #feature
    Change-Id: I300369d36399ffb558b98bafc2478b872d09faba

--- common.mk
+++ common.mk
@@ -95,7 +95,6 @@ PRODUCT_PACKAGES += \
     android.hardware.audio@6.0-impl:32 \
     android.hardware.audio.effect@6.0-impl:32 \
     android.hardware.soundtrigger@2.1-impl:32 \
-    audio.a2dp.default \
     audio.bluetooth.default \
     audio.r_submix.default \
     audio.usb.default \
