66d78ee8a1b528c8e6508249250a700be77e5be1
Omkar Chandorkar <gotenksIN@aosip.dev>
2022-04-07 14:27:06 +0900

    sm8150-common: add hotdogg/guacamoles to build guards
    Signed-off-by: Omkar Chandorkar <gotenksIN@aosip.dev>

--- setup-makefiles.sh
+++ setup-makefiles.sh
@@ -25,7 +25,7 @@ source "${HELPER}"
 setup_vendor "${DEVICE_COMMON}" "${VENDOR}" "${ANDROID_ROOT}" true
 
 # Warning headers and guards
-write_headers "guacamole guacamoleb guacamoleg hotdog hotdogb"
+write_headers "guacamole guacamoleb guacamoleg guacamoles hotdog hotdogb hotdogg"
 
 # The standard common blobs
 write_makefiles "${MY_DIR}/proprietary-files.txt" true
