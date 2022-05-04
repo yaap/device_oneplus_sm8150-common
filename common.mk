22f22b8f59f0149f680811773b472289fa2bc096
John Galt <johngaltfirstrun@gmail.com>
2022-05-04 12:42:42 -0400

    ext4 -> erofs
    Reference:
    https://github.com/aospa-op7/android_device_oneplus_sm8150-common/commit/7c26395196002c0662ade25022eeb911d255888f#

--- common.mk
+++ common.mk
@@ -30,7 +30,7 @@ PRODUCT_ENFORCE_RRO_TARGETS := *
 AB_OTA_POSTINSTALL_CONFIG += \
     RUN_POSTINSTALL_system=true \
     POSTINSTALL_PATH_system=system/bin/otapreopt_script \
-    FILESYSTEM_TYPE_system=ext4 \
+    FILESYSTEM_TYPE_system=erofs \
     POSTINSTALL_OPTIONAL_system=true
 
 PRODUCT_PACKAGES += \
