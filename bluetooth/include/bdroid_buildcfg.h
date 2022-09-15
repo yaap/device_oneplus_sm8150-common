144f147fe454ec88badc80f1f484cc7449036cb9
John Galt <johngaltfirstrun@gmail.com>
2022-09-15 16:01:55 -0400

    Revert "sm8150-common: switch to aosp bluetooth"
    This reverts commit 4b335633a6cc78a3bfd2f04437314f0553474726.

--- bluetooth/include/bdroid_buildcfg.h
+++ bluetooth/include/bdroid_buildcfg.h
@@ -69,9 +69,11 @@ static inline const char *BtmGetDefaultName()
 
 #define BTM_DEF_LOCAL_NAME BtmGetDefaultName()
 // Disables read remote device feature
-#define BTM_WBS_INCLUDED TRUE
-#define BTIF_HF_WBS_PREFERRED TRUE
-
+#define MAX_ACL_CONNECTIONS   16
+#define MAX_L2CAP_CHANNELS    32
 #define BLE_VND_INCLUDED   TRUE
+#define GATT_MAX_PHY_CHANNEL  10
+
+#define AVDT_NUM_SEPS 35
 
 #endif
