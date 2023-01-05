22f85f64ec7578bcb07c409a53eb8294c69e3d67
John Galt <johngaltfirstrun@gmail.com>
2023-01-05 10:45:28 -0500

    Build/use UDFPS sensor

--- common.mk
+++ common.mk
@@ -367,9 +367,13 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/seccomp/mediacodec.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/mediacodec.policy
 
 # Sensors
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/hals.conf:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/hals.conf
+
 PRODUCT_PACKAGES += \
     als_correction_service.oneplus_msmnile \
     android.hardware.sensors@2.1-service.oneplus_msmnile \
+    sensors.oneplus \
     libsensorndkbridge
 
 # Soong namespaces
