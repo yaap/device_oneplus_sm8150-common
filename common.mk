caae45a3c1cf6fab6f5c5f5f11aa7dcc15e6f926
John Galt <johngaltfirstrun@gmail.com>
2023-02-17 13:36:28 -0500

    overlay-yaap: enable clo boostfwk

--- common.mk
+++ common.mk
@@ -9,6 +9,10 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
 # Get non-open-source specific aspects
 $(call inherit-product, vendor/oneplus/sm8150-common/sm8150-common-vendor.mk)
 
+# Get Perf aspects
+$(call inherit-product, device/qcom/common/system/perf/qti-perf.mk)
+$(call inherit-product, device/qcom/common/vendor/perf-legacy/qti-perf-legacy.mk)
+
 # Project ID Quota
 $(call inherit-product, $(SRC_TARGET_DIR)/product/emulated_storage.mk)
 
