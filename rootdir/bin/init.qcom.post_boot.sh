c57708b853956d73e4a53a8b971ba1ae036a1926
Chenyang Zhong <zhongcy95@gmail.com>
2023-03-14 03:05:27 +0600

    sm8150-common: init: Avoid migrating foreground tasks to prime
    The prime core is quite power hungry. Let's only use it for
    top-app tasks. Avoid migrating foreground tasks to prime to reduce
    unnecessary bumps of prime's frequency and save some battery.
    
    Signed-off-by: Chenyang Zhong <zhongcy95@gmail.com>
    Signed-off-by: Vaisakh Murali <mvaisakh@statixos.com>
    
    Updated for our tree, also update background and remove duplicates

--- rootdir/bin/init.qcom.post_boot.sh
+++ rootdir/bin/init.qcom.post_boot.sh
@@ -5282,7 +5282,7 @@ case "$target" in
 		echo 10 > /proc/sys/kernel/sched_group_downmigrate
 		echo 1 > /proc/sys/kernel/sched_walt_rotate_big_tasks
 
-		echo 0-3 > /dev/cpuset/background/cpus
+		echo 0-1 > /dev/cpuset/background/cpus
 		echo 0-3 > /dev/cpuset/system-background/cpus
 
 
