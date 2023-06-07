647f0f73f599cf05b964b16a7fdcd8f396c81970
John Galt <johngaltfirstrun@gmail.com>
2023-06-07 11:54:23 -0400

    common.mk: downgrade task profiles for schedtune

--- common.mk
+++ common.mk
@@ -417,8 +417,8 @@ PRODUCT_SOONG_NAMESPACES += \
 
 # Task Profiles
 PRODUCT_COPY_FILES += \
-    system/core/libprocessgroup/profiles/task_profiles.json:$(TARGET_COPY_OUT_VENDOR)/etc/task_profiles.json \
-    system/core/libprocessgroup/profiles/cgroups.json:$(TARGET_COPY_OUT_VENDOR)/etc/cgroups.json
+    system/core/libprocessgroup/profiles/task_profiles_30.json:$(TARGET_COPY_OUT_VENDOR)/etc/task_profiles.json \
+    system/core/libprocessgroup/profiles/cgroups_30.json:$(TARGET_COPY_OUT_VENDOR)/etc/cgroups.json
 
 # Telephony
 PRODUCT_PACKAGES += \
