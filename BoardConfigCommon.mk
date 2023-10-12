fb8cf15ed99adec5de6ca8fc3c0506ed388133d3
Master <mhmdeveloperofficial@gmail.com>
2023-10-12 17:41:32 +0800

    Disable sparse for ext images
    * Sparse images cause kPostInstallMountError
    
    Co-authored-by: yairpatch <yair.zarfati@gmail.com>
    Change-Id: Ifdb2fc87d88c9c2139b3e73b6ccad36f09888b75
    Signed-off-by: ralph950412 <ralph950412@gmail.com>
    Signed-off-by: Simão Gomes Viana <devel@superboring.dev>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -115,6 +115,9 @@ ODM_MANIFEST_FILES += $(VENDOR_PATH)/manifest-qva.xml
 TARGET_INIT_VENDOR_LIB := //$(VENDOR_PATH):libinit_oneplus-sm8150
 TARGET_RECOVERY_DEVICE_MODULES := libinit_oneplus-sm8150
 
+# Disable sparse for ext images
+TARGET_USERIMAGES_SPARSE_EXT_DISABLED := true
+
 # Metadata
 BOARD_USES_METADATA_PARTITION := true
 
