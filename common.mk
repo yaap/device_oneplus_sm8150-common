b93c4b6fcce9a4dc0cd9fcffe5e022598bf88f7c
Yumi Yukimura <me.cafebabe@gmail.com>
2024-06-24 16:47:29 +0800

    sm8150-common: Migrate to QTI USB Gadget 1.2 HIDL
    Change-Id: I6adbca2fab9a3fe0477d17f9786166828b786710

--- common.mk
+++ common.mk
@@ -448,7 +448,9 @@ PRODUCT_PACKAGES_DEBUG += \
 
 # USB
 PRODUCT_PACKAGES += \
-    android.hardware.usb@1.0-service
+    usb_compositions.conf \
+    android.hardware.usb@1.0-service \
+    android.hardware.usb.gadget@1.2-service-qti
 
 # Vibrator
 PRODUCT_PACKAGES += \
