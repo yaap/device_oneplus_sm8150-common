51e02e627cfd471c796325474f2a80032c791e86
Yumi Yukimura <me.cafebabe@gmail.com>
2024-07-13 23:22:02 +0530

    sm8150-common: Switch to QTI Memtrack AIDL HAL
    Change-Id: Ibcbaff02fd56fbccc3a88c01a5f46a89cb212ef5

--- common.mk
+++ common.mk
@@ -229,8 +229,6 @@ PRODUCT_PACKAGES += \
     android.hardware.graphics.composer@2.4-service \
     android.hardware.graphics.mapper@3.0-impl-qti-display \
     android.hardware.graphics.mapper@4.0-impl-qti-display \
-    android.hardware.memtrack@1.0-impl \
-    android.hardware.memtrack@1.0-service \
     gralloc.qcom \
     hwcomposer.qcom \
     libdisplayconfig.qti \
@@ -239,7 +237,6 @@ PRODUCT_PACKAGES += \
     libqdMetaData \
     libqdMetaData.system \
     libvulkan \
-    memtrack.msmnile \
     vendor.display.config@1.0 \
     vendor.display.config@2.0 \
     vendor.display.config@2.0.vendor \
@@ -247,7 +244,8 @@ PRODUCT_PACKAGES += \
     vendor.qti.hardware.display.mapper@1.1.vendor \
     vendor.qti.hardware.display.mapper@2.0.vendor \
     vendor.qti.hardware.display.mapper@3.0.vendor \
-    vendor.qti.hardware.display.mapper@4.0.vendor
+    vendor.qti.hardware.display.mapper@4.0.vendor \
+    vendor.qti.hardware.memtrack-service
 
 ifneq ($(filter guacamoleb,$(TARGET_DEVICE)),)
 PRODUCT_VENDOR_PROPERTIES += \
