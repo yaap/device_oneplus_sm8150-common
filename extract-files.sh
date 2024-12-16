17cbaf9445d5ae97de858b8d08ed5efb1edaf162
Ivan Vecera <ivan@cera.cz>
2024-12-16 20:16:31 +0100

    sm8150-common: Allow gettid() for atfwd-daemon
    Fixes:
    E ATFWD-daemon: libminijail[11561]: blocked syscall: gettid
    I init    : Service 'vendor.atfwd' (pid 11561) received signal 31
    I init    : Sending signal 9 to service 'vendor.atfwd' (pid 11561) process group...
    
    Change-Id: I637c80cd1ce277902ae9dc5309473370a2acea3d

--- extract-files.sh
+++ extract-files.sh
@@ -86,6 +86,10 @@ function blob_fixup() {
             [ "$2" = "" ] && return 0
             sed -i "s|vendor.boot.verifiedbootstate|vendor.boot.fingerprintbstate|g" "${2}"
             ;;
+        vendor/etc/seccomp_policy/gnss@2.0-qsap-location.policy)
+            [ "$2" = "" ] && return 0
+            grep -q "gettid: 1" "${2}" || echo -e "\ngettid: 1" >> "${2}"
+            ;;
         *)
             return 1
             ;;
