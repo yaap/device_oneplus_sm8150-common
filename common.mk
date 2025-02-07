338c368fe93a5069c0dede5762308621d9abc8aa
mikeNG <mikeioannina@gmail.com>
2025-02-07 15:39:05 +0000

    sm8150-common: Update display flags & packages
    Required after display HAL blueprint conversion

--- common.mk
+++ common.mk
@@ -231,8 +231,8 @@ PRODUCT_PACKAGES += \
     android.hardware.graphics.mapper@4.0-impl-qti-display \
     android.hardware.memtrack@1.0-impl \
     android.hardware.memtrack@1.0-service \
-    gralloc.msmnile \
-    hwcomposer.msmnile \
+    gralloc.qcom \
+    hwcomposer.qcom \
     libdisplayconfig.qti \
     libdisplayconfig.system.qti \
     libgui_vendor \
